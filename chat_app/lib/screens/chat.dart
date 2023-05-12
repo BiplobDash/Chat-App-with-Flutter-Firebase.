53c79191e23d405efc02267146f8bb7ca76a6bac
BiplobDash <biplobdash044@gmail.com>
2023-05-12 21:20:34 +0600

    first commit

---
+++ chat_app/lib/screens/chat.dart
@@ -0,0 +1,54 @@
+import 'package:chat_app/widgets/chat_message.dart';
+import 'package:chat_app/widgets/chat_messagers.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setPushNotification() async{
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    setPushNotification();
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              ))
+        ],
+      ),
+      body: Column(
+        children: const [
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
+      )
+    );
+  }
+}
